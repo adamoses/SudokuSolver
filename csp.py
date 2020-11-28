501d1192b098d54bff6a815dc8a939f7345aee3c
minfante <minfante@vassar.edu>
2020-11-27 22:21:55 -0500

    Kept old csp code for reference, started new csp file that more closely aligns with pseudocode.

--- csp.py
+++ csp.py
@@ -1,92 +1,80 @@
+from copy import Error
 import sudoku
 import queue
 import random
 import util
 
-class SudokuCSP:
+class CSP:
 
     def __init__(self, sudoku):
-
         self.sudoku = sudoku
         self.initial_board = sudoku.board
 
-        self.variables = []         # A list of cell coordinates
-        self.arcs = queue.Queue()
+        self.variables = {}         # A dictionary of cell coordinates
         self.domains = {}           # A dictionary of domains for every variable    
-        self.assignments = {}       # A dictionary of assignments 
+        self.constraints = []       # List of 27 constraints <scope, relation>
         
-        # List of 27 constraints <scope, relation>
-        self.constraints = []   
-
         for y in self.initial_board:
-            t_row = self.sudoku.getRow(y)
-            t_col = self.sudoku.getCol(y)
-            t_box = self.sudoku.getBox(y)
+            t_row = util.abstractRow(y)
+            t_col = util.abstractCol(y)
+            t_box = util.abstractBox(y)
             self.constraints.append((t_row,util.allDiff))
             self.constraints.append((t_col,util.allDiff))
             self.constraints.append((t_box,util.allDiff)) 
             for x in self.initial_board:
-                self.variables.append((x,y))        # Put cell from board in variables
                 if self.initial_board[y][x] == 0:   # For every variable not already assigned give it default domain 
                     self.domains[(x,y)] = range(1,10)  
-                else:                                  # If already assigned give it domain with its value        
+                    self.variables[(x,y)] = False       # Put cell from board in variables  
+                else:                                  # If already assigned give it domain with its value 
                     self.domains[(x,y)] = [self.initial_board[y][x]]
+                    self.variables[(x,y)] = False
+    
+    def checkConstraints(assignment):
+        return True
 
-    def checkConstraints():
-        '''
-        Return the num of constraints violated
-        '''
-        return 0
+class BackTrackingSearch:
 
-    def backTrackingSearch(self):
+    def __init__(self, csp):
+        self.initial_csp = csp
+        self.assignment = csp.initial_board.copy()
+        self.sudoku = csp.sudoku
+    
+    def selectVar(self,csp,assignment):
         '''
-        Alot to do in this function still. Think we're going to run into problems with using self instead of a 
-        physical copy of a csp object. Still, working around it.
+        Eventually implement mrv and degree heuristic
         '''
-        assignments = self.assignments
-        board = self.initial_board
+        mrv = (0,0)
+        return mrv
+
+    def orderValues(self,csp,var,assignment):
+        '''
+        Eventually implement lcv
+        '''
+        val = range(1,10)
+        return val
+
+    def backtrack(self, csp, assignment):
+        if self.sudoku.is_goal(assignment): # if board has properly been assigned, return
+            return assignment
         
-        def selectUnassigned(assignment):
-            return "a"
+        var = self.selectVar(csp, assignment) # select a cell with minimum remaining values in domain
+        x, y = var
+        values = self.orderValues(csp, var, assignment) # order vals by least constraining
+        for value in values:
+            # check if value works on board
+            new_assignment = assignment.copy()
+            new_assignment[x][y] = value
+            if csp.checkConstraints(new_assignment):
+                result = self.backtrack(csp,new_assignment)
+                if result is not None:
+                    return result
+        
+        return None
 
-        def orderDomainValues(var, assignment):
-            return "b"
 
-        def inference(var, assignment):
-            return "c"
 
-        def backtrack(assignment):
-            if assignment.isGoal(): 
-                return assignment
-            var = selectUnassigned(assignment)
-            x, y = var
-            ordered_vals = orderDomainValues(var,assignment)
-            for val in ordered_vals:
-                if not self.checkConstraints(val): # this also doesnt work (pseudocode)
-                    prev = assignment[y][x]
-                    assignment[y][x] = val
-                    inferences = inference(var,assignment)
-                    if inferences: # if inferences were correct (forward checking)
-                        new_domain = self.domains
-                        new_domain[var] = [val]
-
-                        result = backtrack(assignment.copy()) 
-                        if result is not None:
-                            return result
-                        
-                    assignment[y][x] = prev  # reset asssignment if it didnt work 
-                    
-            return None
-
-        return backtrack(board.copy())
-
-    def minConflictSearch(self):
-        return "Goomah"
-
-    def humanLikeSearch(self):
-        return "Ohhhhh!"
-
+    def search(self, csp):
+        return self.backtrack(csp, self.assignment.copy())
+        
     
 
-
-        
